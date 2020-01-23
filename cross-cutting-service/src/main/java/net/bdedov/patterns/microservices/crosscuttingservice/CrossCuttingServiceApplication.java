f63c08ff5f7057583b7e1586ae1024f4ed38070b
bdedov <bozhidar.dedov@gmail.com>
2020-01-23 19:43:38 +0200

    Added Cross Cutting Pattern: Externalized Configuration

---
+++ cross-cutting-service/src/main/java/net/bdedov/patterns/microservices/crosscuttingservice/CrossCuttingServiceApplication.java
@@ -0,0 +1,12 @@
+package net.bdedov.patterns.microservices.crosscuttingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class CrossCuttingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossCuttingServiceApplication.class, args);
+	}
+
+}
---
+++ cross-cutting-service/src/test/java/net/bdedov/patterns/microservices/crosscuttingservice/CrossCuttingServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package net.bdedov.patterns.microservices.crosscuttingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class CrossCuttingServiceApplicationTests {
+
+	@Test void contextLoads() {
+	}
+
+}
